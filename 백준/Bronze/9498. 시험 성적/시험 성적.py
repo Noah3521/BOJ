739f764e98e402865d6bb82bf2dacc929361e1b0
Noah3521 <115360587+Noah3521@users.noreply.github.com>
2023-05-10 08:27:55 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+test = int(input())
+
+if test >= 90 :
+    result = "A"
+elif test >= 80:
+    result = "B"
+elif test >= 70:
+    result = "C"
+elif test >= 60:
+    result = "D"
+else:
+    result = "F"
+
+print(result)
