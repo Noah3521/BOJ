9e6dfda885aca9b4db6e772ac4219d5374b19eaf
Noah3521 <115360587+Noah3521@users.noreply.github.com>
2023-05-13 22:02:55 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 1380 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -3,91 +3,84 @@
 #include <string.h>
 
 typedef struct stack {
-	int data;
-	struct stack* link;
+    int data;
+    struct stack* link;
 }Stack;
 
-Stack* getStack(int n)
-{
-	Stack* node = (Stack*)malloc(sizeof(Stack));
-	node->data = n;
-	node->link = NULL;
-	return node;
+Stack* getStack(int n) {
+    Stack* node = (Stack*)malloc(sizeof(Stack));
+    node->data = n;
+    node->link = NULL;
+    return node;
 }
 
-int count = 0;
+int size = 0;
 
-void push(Stack** top, int n)
-{
-	Stack* tmp = *top;
-	*top = getStack(n);
-	(*top)->link = tmp;
-	count++;
+void push(Stack** top, int n) {
+    Stack* tmp = *top;
+    *top = getStack(n);
+    (*top)->link = tmp;
+    //printf("%d추가\n", n);
+    size++;
 }
 
-void top1(Stack* top)
-{
-	if (top == NULL)
-	{
-		printf("%d\n", -1);
-		return;
-	}
-	printf("%d\n", top->data);
+void pop(Stack** top) {
+    if((*top)==NULL){
+        printf("-1\n");
+        return;
+    }
+    int data = -1;
+    Stack* tmp = *top;
+    data = (*top)->data;
+    *top = tmp->link;
+    printf("%d\n",data);
+    free(tmp);
+    size--;
 }
 
-void pop(Stack** top)
-{
-	if (*top == NULL)
-	{
-		printf("%d\n", -1);
-		return;
-	}
 
-	int data = 0;
-	Stack* tmp = *top;
-	data = (*top)->data;
-	(*top) = tmp->link;
-	free(tmp);
-	count--;
-	printf("%d\n", data);
+void high(Stack* top) {
+    if(top==NULL){
+        printf("-1\n");
+        return;
+    }
+    printf("%d\n", top->data);
 }
 
-void empty(Stack* top)
+int main()
 {
-	if (top == NULL)
-		printf("%d\n", 1);
-	else printf("%d\n", 0);
+    /*
+    push x
+    pop
+    size
+    empty
+    top
+    */
+    Stack* top = NULL;
+    char menu[6] = "";
+    int N = 0;
+    scanf("%d", &N);
+    
+    for(int i=0;i<N;i++) {
+    scanf("%s", menu);
+    if(strcmp(menu, "push")==0){
+        int tmp = 0;
+        scanf("%d", &tmp);
+        push(&top, tmp);
+    }
+    else if(strcmp(menu, "pop")==0){
+        pop(&top);
+    }
+    else if(strcmp(menu, "top")==0) {
+        high(top);
+    }
+    else if(strcmp(menu, "empty")==0) {
+        top==NULL ? printf("1\n") : printf("0\n");
+    }
+    else if(strcmp(menu, "size")==0) {
+        printf("%d\n",size);
+    }
+    }
+    return 0;
 }
 
-int main(void)
-{
-	Stack* top = NULL;
-	char menu[6] = "";
-	int remit = 0;
-	scanf("%d", &remit);
-
-	for (int i = 0; i < remit; i++)
-	{
-		scanf("%s", menu);
-		if (strcmp(menu, "push") == 0)
-		{
-			int n = 0;
-			scanf("%d", &n);
-			push(&top, n);
-		}
-
-		else if (strcmp(menu, "pop") == 0)
-			pop(&top);
-
-		else if (strcmp(menu, "top") == 0)
-			top1(top);
-
-		else if (strcmp(menu, "size") == 0)
-			printf("%d\n", count);
-
-		else if (strcmp(menu, "empty") == 0)
-			empty(top);
-
-	}
-	return 0;
-}
