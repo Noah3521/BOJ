4fde51f8d1e460ceffb06cb35fd8b707858c891c
Noah3521 <115360587+Noah3521@users.noreply.github.com>
2023-05-16 05:22:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,18 @@
+H = [int(input()) for _ in range(9)]
+
+H.sort()
+
+N = sum(H) - 100
+
+# print(N)
+# print(H)
+for i in range(len(H)):
+    for j in range(i+1, len(H)):
+        if(H[i] + H[j] == N):
+                remove1 = H[i]
+                remove2 = H[j]
+H.remove(remove1)
+H.remove(remove2)
+
+for i in H:
+     print(i)
