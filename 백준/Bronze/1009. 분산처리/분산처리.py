e0ed6e6c6149413aaf7d82f9bfdb48cbb89b4ef4
Noah3521 <115360587+Noah3521@users.noreply.github.com>
2023-06-16 03:08:10 +0900

    [Bronze II] Title: 분산처리, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+computer = 10
+
+dict = {
+    0:[10],
+    1:[1],
+    2:[2,4,8,6],
+    3:[3,9,7,1],
+    4:[4,6],
+    5:[5],
+    6:[6],
+    7:[7,9,3,1],
+    8:[8,4,2,6],  
+    9:[9,1] 
+}
+
+for i in range(N):
+    a, b = map(int, input().split())
+    a%=10
+    print(dict[a][b%len(dict[a])-1])
